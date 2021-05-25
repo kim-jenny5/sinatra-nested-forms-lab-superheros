f32ad9f5f5328bb2a786cb1b0ec9e8c1bbf56479
Jenny Kim <kimj1905@gmail.com>
2021-05-24 20:33:31 -0400

    commit tree a7c5482b53a40e7b4a4159996bad15755e46a11e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        @team = params[:team][:name]
+        @team_motto = params[:team][:motto]
+
+        @names = []
+        @powers = []
+        @bios = []
+
+        @heroes = params[:team][:members].each do |member|
+            @names << member[:name]
+            @powers << member[:power]
+            @bios << member[:bio]
+        end
+
+        erb :team
+    end
 
 end
